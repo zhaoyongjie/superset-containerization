075fca24ced180c2f725321ff352e18494b0ac86
Yongjie Zhao <yongjie.zhao@gmail.com>
2023-01-18 22:49:31 +0800

    initial commit

---
+++ superset_home/superset_config.py
@@ -0,0 +1,71 @@
+import logging
+import os
+from typing import Optional
+
+
+logger = logging.getLogger()
+
+
+def get_env_variable(var_name: str, default: Optional[str] = None) -> str:
+    """Get the environment variable or raise exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        if default is not None:
+            return default
+        else:
+            error_msg = "The environment variable {} was missing, abort...".format(
+                var_name
+            )
+            raise EnvironmentError(error_msg)
+
+
+DATABASE_DIALECT = get_env_variable("DATABASE_DIALECT")
+DATABASE_USER = get_env_variable("DATABASE_USER")
+DATABASE_PASSWORD = get_env_variable("DATABASE_PASSWORD")
+DATABASE_HOST = get_env_variable("DATABASE_HOST")
+DATABASE_PORT = get_env_variable("DATABASE_PORT")
+DATABASE_DB = get_env_variable("DATABASE_DB")
+
+# The SQLAlchemy connection string.
+SQLALCHEMY_DATABASE_URI = "%s://%s:%s@%s:%s/%s" % (
+    DATABASE_DIALECT,
+    DATABASE_USER,
+    DATABASE_PASSWORD,
+    DATABASE_HOST,
+    DATABASE_PORT,
+    DATABASE_DB,
+)
+
+REDIS_HOST = get_env_variable("REDIS_HOST")
+REDIS_PORT = get_env_variable("REDIS_PORT")
+
+CACHE_CONFIG = {
+    "CACHE_TYPE": "redis",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_KEY_PREFIX": "superset_",
+    "CACHE_REDIS_HOST": REDIS_HOST,
+    "CACHE_REDIS_PORT": REDIS_PORT,
+}
+FILTER_STATE_CACHE_CONFIG = CACHE_CONFIG
+EXPLORE_FORM_DATA_CACHE_CONFIG = CACHE_CONFIG
+
+# Security
+SECRET_KEY = "HbJUf96F9swUYq5YCBOUZ3clkYQFqSTm6AJbL97T9ZVkSAO+XzVuZae2"
+CONTENT_SECURITY_POLICY_WARNING = False
+
+# Feature Flags
+FEATURE_FLAGS = {
+    "DISABLE_LEGACY_DATASOURCE_EDITOR": False,
+
+    "DASHBOARD_NATIVE_FILTERS": True,
+    "DASHBOARD_CROSS_FILTERS": True,
+    "DASHBOARD_NATIVE_FILTERS_SET": True,
+    "DASHBOARD_RBAC": True,
+
+    "ENABLE_TEMPLATE_PROCESSING": True,
+    "UX_BETA": True,
+    "GENERIC_CHART_AXES": True,
+    "DRILL_TO_DETAIL": True,
+    "DATAPANEL_CLOSED_BY_DEFAULT": True,
+}
